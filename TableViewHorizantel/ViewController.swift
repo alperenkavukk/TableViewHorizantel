b67ca6e11473df025eb9b4111ef961798e6395c9
alperenkavukk <kavuk.67.76@gmail.com>
2023-07-04 14:17:00 +0300

    Initial Commit

---
+++ TableViewHorizantel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewHorizantel
+//
+//  Created by Alperen Kavuk on 4.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
